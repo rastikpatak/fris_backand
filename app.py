332daf8db975372729b1d63349d22c881a97b1c9
rastikpatak <rastikpatak@gmail.com>
2026-03-30 09:26:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 database = {
     'students': [
-        {"id": 1, "name": "samuel", "surname": "martis", "img": "data:image/webp;base64,UklGRtAMAABXRUJQVlA4IMQMAAD…aXjYzlRmBLbhHC7bMAeXYRFKK8LgHgHAMg54+qZBkE6OYAAAA"},
-        {"id": 2, "name": "andrej", "surname": "bucko", "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:AN…zd5q6xrMx8H-ytLFhiaVTPCzvMF435VHWdB80bF_mIdY&s=10"},
+        {"id": 1, "name": "samuel", "surname": "martis", "img": "http://www.gcm.sk/images/logo.jpg"},
+        {"id": 2, "name": "andrej", "surname": "bucko", "img": "http://www.gcm.sk/images/logo.jpg"},
         {"id": 3, "name": "rasto", "surname": "patak", "img": ""},
         {"id": 4, "name": "martin", "surname": "cepcek", "img": " "},
         {"id": 5, "name": "peter", "surname": "marcin", "img": " "},
@@ -26,7 +26,7 @@ def find_student(id):
     return jsonify(student)
 @app.route('/str')
 def pusti_stranku():
-    return render_template("index.html")
+    return render_template("index2.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
