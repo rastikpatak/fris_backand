a287a96f3fa21c70c0d368a8ee2106938029486a
rastikpatak <rastikpatak@gmail.com>
2026-04-16 08:31:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 database = {
     'students': [
         {"id": 1, "name": "samuel", "surname": "martis", "img": "http://www.gcm.sk/images/logo.jpg"},
