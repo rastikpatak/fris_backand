bcd3bdcfb4c136c599d983f59f77a67d42f63208
rastikpatak <rastikpatak@gmail.com>
2026-04-27 08:59:18 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 import psycopg2
 
 conn = psycopg2.connect(
-    host="dpg-d7ng5n2qqhas73frva90-a",
-    database="PostgreSQL",
+    host="dpg-d7ng5n2qqhas73frva90-a.frankfurt-postgres.render.com",
+    database="postgres_3eru",
     user="postgres_3eru_user",
     password="mOnYuHR6gLCRFzYhin9DSFpaTmd0lu9l",
-    port= 5432
+    port= 5432,
 )
 
 cursor = conn.cursor()
