f4d7152d4f8585f909f4455c32ced18ffcb919f7
rastikpatak <rastikpatak@gmail.com>
2026-04-27 08:55:17 +0200

    Fix database connection host format

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 import psycopg2
 
 conn = psycopg2.connect(
-    host="dpg-d7ng5n2qqhas73frva90-a/postgres_3eru",
+    host="dpg-d7ng5n2qqhas73frva90-a",
     database="PostgreSQL",
     user="postgres_3eru_user",
     password="mOnYuHR6gLCRFzYhin9DSFpaTmd0lu9l",
-    port="5432"
+    port= 5432
 )
 
 cursor = conn.cursor()
